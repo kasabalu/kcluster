f3cde4872f1dd2c1b8442bfa077ea2bebb6cb9be
bkasa724 <balakrishna_kasa@comcast.com>
2022-06-16 12:38:09 -0400

    informers and Listers genarators

--- main.go
+++ main.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"github.com/kasabalu/kcluster/pkg/apis/kasabalu.dev/v1alpha1"
 )
-func main(){
-	k:= v1alpha1.Kluster{}
+
+func main() {
+	k := v1alpha1.Kluster{}
 	fmt.Println(k)
 
 }
